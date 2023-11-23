00d53b27111ccb78adc9eac0db9e69d5932f525a
chrnivan <chnivan@mail.ru>
2023-11-23 16:42:04 +0300

    изменение теста testFeed()

--- ImageFeedUITests/ImageFeedUITest.swift
+++ ImageFeedUITests/ImageFeedUITest.swift
@@ -21,7 +21,7 @@ final class ImageFeedUITest: XCTestCase {
         
         app.launchArguments = ["testMode"]
         app.launch()
-       
+        
     }
     
     func testAuth() throws {
@@ -55,26 +55,28 @@ final class ImageFeedUITest: XCTestCase {
     }
     
     func testFeed() throws {
-        
-        
-        
-        
         let tablesQuery = app.tables
         
         let cell = tablesQuery.children(matching: .cell).element(boundBy: 0)
         cell.swipeUp()
         
+        sleep(3)
+        
+        let cellToLike = tablesQuery.descendants(matching: .cell).element(boundBy: 1)
+        
+        let likeButton = cellToLike.descendants(matching: .button).element(boundBy: 0)
+        
+        likeButton.tap()
+        
         sleep(2)
         
-        let cellToLike = tablesQuery.children(matching: .cell).element(boundBy: 1)
-        
-        cellToLike.buttons["like button"].tap()
-        sleep(2)
-        cellToLike.buttons["like button"].tap()
+        likeButton.tap()
         
         sleep(2)
+        
         cellToLike.tap()
-        sleep(2)
+        
+        sleep(3)
         
         let image = app.scrollViews.images.element(boundBy: 0)
         image.pinch(withScale: 3, velocity: 1)
